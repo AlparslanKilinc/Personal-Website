c52aad0c2526335cce2417ae6ea83a0239a83379
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-04-30 16:25:07 -0400

    Initial Set up
        Set up with Vite, Created Folders for components and css
        Added reset.css for overwriting styles. Planing to make components
        for each section and put them together in App.jsx.
        This Personal Website will be inspired by Github Profile section.
        Added similar colors to github and working on creating a navbar.

---
+++ personal-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './css/reset.css'
+import  Navbar from './components/navbar.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <p>Hello World !</p>
+    </>
+  )
+}
+
+export default App
---
+++ personal-project/src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import '../css/navbar.css'
+function Navbar() {
+
+    return (
+      <>
+      <li className='nav-list'>
+        <button className='nav-button'>Projects</button>
+        <button className='nav-button'>Education</button>
+        <button className='nav-button'>About Me</button>
+        <button className='nav-button'>Awards</button>
+      </li>
+      </>
+    )
+  }
+  
+  export default Navbar
