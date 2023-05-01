24a4db096baa22886923959d0b406745e376f1e2
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-05-01 01:11:34 -0400

    Added Awards Section
        Included the dates and some information about the awards.
        Pretty much done now. Will work on responsive design next.
        Maybe some info adjustments in the future to update.

--- personal-project/src/components/aboutme.jsx
+++ personal-project/src/components/aboutme.jsx
@@ -7,15 +7,12 @@ function AboutMe() {
     <div className='aboutMe'>
       <p className='title'>About Me</p>
         <div className='about-box'>
-          <img className='about-img' src="\src\assets\turkiye.png" alt="Turkiye Flag"/>
+          <img className='flag-img' src="\src\assets\turkiye.png" alt="Turkiye Flag"/>
           <div className='box-info'>
             <h1 className='uni-title'>My Origins</h1>
-            <p>I am from Turkiye,Istanbul.Istanbul is the city of cats where i grew up with love for cats.
-               I started playing soccer from a young age in Turkiye. After moving to United States 
-               I played Tennis and Soccer Throughout High-School. 
-               I always loved Art and Making things beautiful which lead me to get into Front-End Development.
-               Where i can mix Design,Art and Programming.
-            </p>
+              <p> I am from Turkiye,Istanbul.Istanbul is the city of cats where i grew up with love for cats.</p>
+              <p> After moving to United States I played Tennis and Soccer Throughout High-School. </p>
+              <p> I always loved Art and making things beautiful which lead me to get into Front-End Development.</p>
           </div>
         </div>
 
@@ -23,14 +20,11 @@ function AboutMe() {
           <img className='about-img' src="\src\assets\react.png" alt="React Logo"/>
           <div className='box-info'>
             <h1 className='box-title'>Programming Journey</h1>
-            <p> 
-              After high-school I started pursing Computer Science. 
-              I started studying Computer Science at Suffolk Community College and graduated with a 4.0 gpa.
-              Along the way I learned Object-Oriented Programming by making projects. 
-              After Transferring to Oswego University I started focusing on Web Development.
-              In the end I ended up at Stony Brook University where 
-              I will be graduating soon with Bachelors in Computer Science.
-            </p>
+            <p>After high-school I started pursing Computer Science.</p>
+            <p>I started studying Computer Science at Suffolk Community College and graduated with a 4.0 gpa.</p>
+            <p>Along the way I learned Object-Oriented Programming by making projects.</p>
+            <p>After Transferring to Oswego State University I started focusing on Web Development.</p>
+            <p>In the end I ended up at Stony Brook University where I will be graduating soon.</p>
           </div>
         </div>
         <p className='title'>Skills</p>
--- personal-project/src/components/awards.jsx
+++ personal-project/src/components/awards.jsx
@@ -5,7 +5,24 @@ import '../css/reset.css'
 function Awards() {
   return (
     <div className='awards'>
-        <p>Awards</p>
+        <div className='award-box'>
+          <img className='award-img' src="\src\assets\nsf.png" alt="National Science Foundation Logo"/>
+          <div className='award-info'>
+            <h1 className='award-title'>National Science Foundation Scholarship</h1>
+            <p>Aug 2020</p>
+            <p>The NSF STEM Scholarship program at Suffolk County Community</p>
+            <p> College provides a unique, educational enrichment environment for students pursuing</p> 
+            <p>careers in science technology, engineering, and mathematics fields</p>
+          </div>
+        </div>
+        <div className='award-box'>
+          <img className='award-img' src="\src\assets\suffolk.png" alt="Suffolk Logo"/>
+          <div className='award-info'>
+            <h1 className='award-title'>Dr. Frey Family Foundation Scholarship</h1>
+            <p>Dec 2021</p>
+            <p>Recognized for Mathematical Achievements at Suffolk County Community College.</p>
+          </div>
+        </div>
     </div>
   )
 }
