c07bc5b0b5f9825c6e6d93a39f6717d39cb2c91e
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-04-30 22:56:38 -0400

    Add Education component     Added all the school i attended.     I am not sure what else i can include.     Maybe will update later.

--- personal-project/src/components/education.jsx
+++ personal-project/src/components/education.jsx
@@ -5,7 +5,33 @@ import '../css/reset.css'
 function Education() {
   return (
     <div className='education'>
-        <p>Education</p>
+        <div className='uni-box'>
+          <img src="\src\assets\sbu.png" alt="Stony Brook University Logo"/>
+          <div className='uni-info'>
+            <h1 className='uni-title'>Stony Brook University</h1>
+            <p>Major: Computer Science</p>
+            <p>GPA:3.6</p>
+          </div>
+        </div>
+
+        <div className='uni-box'>
+          <img src="\src\assets\oswego.png" alt="Oswego Logo"/>
+          <div className='uni-info'>
+            <h1 className='uni-title'>Oswego State University</h1>
+            <p>Major: Computer Science</p>
+            <p>GPA:3.8</p>
+          </div>
+        </div>
+
+        <div className='uni-box'>
+          <img src="\src\assets\suffolk.png" alt="Suffolk Logo"/>
+          <div className='uni-info'>
+            <h1 className='uni-title'>Suffolk County Community College</h1>
+            <p>Major: Computer Science</p>
+            <p>GPA:4.0</p>
+          </div>
+        </div>
+
     </div>
   )
 }
