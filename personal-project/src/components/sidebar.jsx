b3c7ebfb897902853df78eeb20240cdee5845a1c
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-04-30 20:24:32 -0400

    Sidebar Added     Added Avatar and links to linkedin, resume,mail github     Try to align things together but i am not sure if i it is proper.     Going to move on to , loading contents using the navbar.     Thinking of making the navbar  load components depending on click     and the state.

--- personal-project/src/components/sidebar.jsx
+++ personal-project/src/components/sidebar.jsx
@@ -1,13 +1,48 @@
 import '../css/sidebar.css'
+import '../css/reset.css'
 import React, { useContext, useEffect,useState } from 'react'
 import Avatar from '@mui/material/Avatar';
 
 function Sidebar() {
+    const style = {
+        border:'2px solid #f0f3f6',
+        width:'300px',
+        height:'300px'
+      };
 
     return (
       <div className='sidebar'>
-        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        <p>SideBar</p>
+        <Avatar sx={style} alt="Profile Pic" src="\src\assets\profile_pic.jpg"/>
+        <div className='personal-info'>
+                <h1>Alparslan Kilinc</h1>
+                <div className='text-info'>
+                    <p >Stony Brook University,</p>
+                    <p> B.S. Computer Science.</p>
+                    <p> Software Development.</p>
+                </div>
+            <ul className='contact-info'>
+                <li style={{display:'flex',gap:'5px',alignItems:'center'}}>
+                    <i className="material-icons-outlined">location_on</i>
+                    <p>New York City, New York</p>
+                </li>
+                <li style={{display:'flex',gap:'5px',alignItems:'center'}}>
+                    <i className="fa-brands fa-linkedin" style={{marginLeft:'5px'}}></i>
+                    <a href="https://www.linkedin.com/in/alparslan-kilinc-239358202/" target="_blank">LinkedIn</a>
+                </li>
+                <li style={{display:'flex',gap:'5px',alignItems:'center'}}>
+                    <i className="material-icons-outlined">file_open</i>
+                    <a href="\src\assets\Kilinc_Resume.pdf" target="_blank">Resume</a>
+                </li>
+                <li style={{display:'flex',gap:'5px',alignItems:'center'}}>
+                    <i className="material-icons-outlined">email</i>
+                    <a href="mailto: kilincalparslan70@gmail.com" target="_blank">Mail</a>
+                </li>
+                <li style={{display:'flex',gap:'5px',alignItems:'center'}}>
+                    <i className="fa-brands fa-github"></i>
+                    <a href="https://github.com/AlparslanKilinc" target="_blank">Github</a>
+                </li>
+            </ul>
+        </div>
       </div>
     )
   }
